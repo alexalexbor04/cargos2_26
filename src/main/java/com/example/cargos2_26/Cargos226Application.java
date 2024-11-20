54b040e468f1df48e71416c14c7111662de34011
Sasha <alexalexbor04@gmail.com>
2024-11-20 23:40:18 +0300

    Initial commit

---
+++ src/main/java/com/example/cargos2_26/Cargos226Application.java
@@ -0,0 +1,13 @@
+package com.example.cargos2_26;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cargos226Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cargos226Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cargos2_26/Cargos226ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cargos2_26;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cargos226ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
